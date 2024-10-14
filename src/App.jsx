88acbe2e25e95f75863d2a1304715fb1a44143bd
RosarioManny <rosario.emm47@gmail.com>
2024-10-14 15:29:40 -0400

    Set money and team useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from 'react'
+import { characters } from './data.js';
 
 const App = () => {
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100)
+  
+  console.log(money)
+  console.log(team)
 
   return (
+
     <h1>Hello world!</h1>
   );
 }
