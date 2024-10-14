b9269b4c344977afcd22b2c63735a0d7fa9b9362
RosarioManny <rosario.emm47@gmail.com>
2024-10-14 16:15:51 -0400

    Listed Figther Info

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,32 @@ import { characters } from './data.js';
 const App = () => {
   const [team, setTeam] = useState([]);
   const [money, setMoney] = useState(100)
-  
-  console.log(money)
-  console.log(team)
+  const [zombieFighters, setZombieFighters] = useState(characters)
+
+  const listFighter = zombieFighters.map((fighter) => {
+    return(
+      <li>
+      {fighter.name} :: <b>Cost - {fighter.price} </b>
+        <p>
+        Agility: {fighter.agility} 
+        <br />
+        Strength: {fighter.strength}
+        </p> 
+        <img src={fighter.img} alt="link broken :(" />
+        <button type="submit">Add</button>
+      </li>
+    )
+  })
 
   return (
-
-    <h1>Hello world!</h1>
+    <>
+     <h1>ReactVille IN PERIL!</h1>
+     <p>Current Cash: <b>${money}</b></p>
+     <ul>
+      {listFighter}
+     </ul>
+    </>
+   
   );
 }
 
