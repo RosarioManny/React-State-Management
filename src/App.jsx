807a9dcd4b8fa93bdf89cf18f0f88a5bd0dd8673
RosarioManny <rosario.emm47@gmail.com>
2024-10-21 19:53:48 -0400

    Team Updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import { characters } from './data.js';
+import './App.css'
+import './index.css'
 
 const App = () => {
   const [team, setTeam] = useState([]);
-  const [money, setMoney] = useState(100)
-  const [zombieFighters, setZombieFighters] = useState(characters)
+  const [money, setMoney] = useState(100);
+  const [zombieFighters, setZombieFighters] = useState(characters);
+  const [teamStrength, setTeamStrength] = useState(0);
+  const [teamAgility, setTeamAgility] = useState(0);
 
   const listFighter = zombieFighters.map((fighter, i) => {
     return(
@@ -22,52 +26,65 @@ const App = () => {
     )
   })
 
-  const handleAddFighter = (addedMem) => {
 
-    // Checks if you can Afford
-    const moneyDifference = money - addedMem.price
-      if(addedMem.price > money) {
-        console.log("Need More Cash")
-        return <p>"Need enought cash"</p>
-      } else {
-        setMoney(moneyDifference)
-      }
-    // This adds figther
-    const addedFighter = [...team, addedMem];
-    setTeam(addedFighter);
-    
-    // Removes fighter from List of Options
-    const figtherId = zombieFighters.indexOf(addedMem);
-    const removeFighter = zombieFighters.splice(figtherId, 1)
-    // setZombieFighters(zombieFigthers removeFighter)
-  }
-
-  const handleTeam = () => {
-    if(team.length = 0) {
-        <p>Pick Some Team Members</p>
-    } 
-    else { team.map((teamMember) => (
-       return(
-       <p key={teamMember.name}>
-        <b>{teamMember.name}</b> :: Strength: {teamMember.strength} || Agility: {teamMember.agility}
-      </p> ) 
-      ))
+  const handleAddFighter = (fighter) => {
+    if (money >= fighter.price) {
+      const moneyDifference = money - fighter.price
+      setMoney(moneyDifference);
+      setTeam([...team, fighter]);
+      setTeamAgility(teamAgility + fighter.agility)
+      setTeamStrength(teamStrength + fighter.strength)
+    } else {
+      console.log("Not Enough money")
     }
   }
+
+  const listTeam = team.map((fighter, i) => {
+    return(
+      <li key={i}>
+      {fighter.name} :: <b>Cost - {fighter.price} </b>
+        <p>
+        Agility: {fighter.agility} 
+        <br />
+        Strength: {fighter.strength}
+        </p> 
+        <img src={fighter.img} alt="link broken :(" />
+      </li>
+    )
+  })
+
+
+
+  // const listTeam = team.map((member, i) => {
+  //   return (
+  //    <li key={i}>
+  //    {member.name} :: <b>Cost - {member.price} </b>
+  //      <p>
+  //      Agility: {member.agility} 
+  //      <br />
+  //      Strength: {member.strength}
+  //      </p> 
+  //      <img src={member.img} alt="link broken :(" />
+  //    </li>
+  //   )
+  //  })
+
   
   return (
     <>
      <h1>ReactVille IN PERIL!</h1>
-     <h2>Current Cash: <b>${money}</b></h2>
+     <h2>Current Cash: <b>{money}</b></h2>
+     <h2>Agility: {teamAgility}</h2>
+     <h2>Strength: {teamStrength}</h2>
      <h2>Your Team</h2>
-     <p>{handleTeam()}</p>
      <ul>
+       {listTeam}
      </ul>
+  
      <hr/>
      <ul>
       {listFighter}
      </ul>
-     
     </>
    
   );
